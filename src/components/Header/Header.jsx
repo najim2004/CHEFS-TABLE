5e28e589932954a6702ec9c0e9152ad0c582e74b
najim2004 <najim10112004@gmail.com>
2024-03-15 21:29:53 +0600

    Added navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+
+import PropTypes from 'prop-types';
+import Navbar from '../Navbar/Navbar';
+
+const Header = props => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
